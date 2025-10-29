a6ebdd577d9597bf17b03879c5e01fd49b68bce5
Daniel <166452209+star-Daniels@users.noreply.github.com>
2025-10-29 15:47:36 -0300

    ADD

--- src/test/java/ProductApplicationTest.java
+++ src/test/java/ProductApplicationTest.java
@@ -1,5 +1,6 @@
 import com.example.applications.ProductApplication;
 import com.example.services.ProductService;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
@@ -10,4 +11,19 @@ public class ProductApplicationTest {
 
     @Mock
     ProductService productService;
+
+    @Test
+    public void deveAdicionarUmaImagem(){
+
+    }
+
+    @Test
+    public void deveRemoverAImagem(){
+
+    }
+
+    @Test
+    public void deveAtualizarAImagem(){
+        
+    }
 }
